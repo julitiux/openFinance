6a8470a2de01d506ac51285c4bf629a3297d462c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-26 10:59:34 -0600

    delete all about paramters from methods reverseStringList and filter

--- lib/src/main/java/actividad1/Actividad1.java
+++ lib/src/main/java/actividad1/Actividad1.java
@@ -19,17 +19,12 @@ public class Actividad1 {
     return getResult();
   }
 
-  public List<String> actividad(List<String> stringList) {
-    List<String> listFilter = filter(stringList);
-    return reverseStringList(listFilter);
+  private List<String> filter() {
+    return result.stream().filter(string -> string.contains("y")).collect(Collectors.toList());
   }
 
-  private List<String> filter(List<String> stringList) {
-    return stringList.stream().filter(string -> string.contains("y")).collect(Collectors.toList());
-  }
-
-  private List<String> reverseStringList(List<String> stringList) {
-    return stringList.stream().map(string -> new StringBuilder(string).reverse().toString()).collect(Collectors.toList());
+  private List<String> reverseStringList() {
+    return result.stream().map(string -> new StringBuilder(string).reverse().toString()).collect(Collectors.toList());
   }
 
 }
