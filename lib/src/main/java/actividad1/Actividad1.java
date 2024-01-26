6af284485a6251a26413544539adfe72aa3af127
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-26 11:36:16 -0600

    implement constructor and add paramter stringList into executeActividad method

--- lib/src/main/java/actividad1/Actividad1.java
+++ lib/src/main/java/actividad1/Actividad1.java
@@ -8,19 +8,23 @@ public class Actividad1 {
 
   public static List<String> result;
 
-  public Actividad1 setResult(List<String> result) {
+  public Actividad1 (List<String> result) {
     Actividad1.result = result;
-    return this;
   }
 
   public static List<String> getResult() {
     return result;
   }
 
-  public static List<String> executeActividad(Consumer<Actividad1> consumer) {
-    Actividad1 actividad1 = new Actividad1();
+  public static List<String> executeActividad(Consumer<Actividad1> consumer, List<String> stringList) {
+    Actividad1 actividad1 = new Actividad1(stringList);
     consumer.accept(actividad1);
     return getResult();
+  }
+
+  public Actividad1 distinctList() {
+    result = result.stream().distinct().collect(Collectors.toList());
+    return this;
   }
 
   public Actividad1 filter() {
