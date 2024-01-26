583405488c623194f24a8448674c68314cf1720b
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-26 11:29:32 -0600

    execute the method like I want

--- lib/src/test/java/actividad1/Actividad1Test.java
+++ lib/src/test/java/actividad1/Actividad1Test.java
@@ -1,4 +1,22 @@
 package actividad1;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Actividad1Test {
+
+  @Test
+  public void test() {
+    List<String> result =
+      Actividad1.executeActividad(
+        methods -> methods
+          .setResult(Arrays.asList("Hello", "Hello", "Bye", "World"))
+          .filter()
+          .reverseStringList()
+      );
+    System.out.println(result);
+  }
+
 }
