35bc9be99674ac78c9b296e16f0fcba7fb7d8c25
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-26 11:28:52 -0600

    modify access methods and return the same class

--- lib/src/main/java/actividad1/Actividad1.java
+++ lib/src/main/java/actividad1/Actividad1.java
@@ -1,30 +1,36 @@
 package actividad1;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Actividad1 {
 
-  static List<String> result = new ArrayList<String>();
+  public static List<String> result;
+
+  public Actividad1 setResult(List<String> result) {
+    Actividad1.result = result;
+    return this;
+  }
 
   public static List<String> getResult() {
     return result;
   }
 
-  public static List<String> executeActividad(Consumer<Actividad1> consumer){
+  public static List<String> executeActividad(Consumer<Actividad1> consumer) {
     Actividad1 actividad1 = new Actividad1();
     consumer.accept(actividad1);
     return getResult();
   }
 
-  private List<String> filter() {
-    return result.stream().filter(string -> string.contains("y")).collect(Collectors.toList());
+  public Actividad1 filter() {
+    result = result.stream().filter(string -> string.contains("y")).collect(Collectors.toList());
+    return this;
   }
 
-  private List<String> reverseStringList() {
-    return result.stream().map(string -> new StringBuilder(string).reverse().toString()).collect(Collectors.toList());
+  public Actividad1 reverseStringList() {
+    result = result.stream().map(string -> new StringBuilder(string).reverse().toString()).collect(Collectors.toList());
+    return this;
   }
 
 }
