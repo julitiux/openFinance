d5e5a0d50eaadbc62beb73ffbcf5057a79a6655a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-26 10:41:07 -0600

    implement method reverseStringList and apply it into method activdad

--- lib/src/main/java/actividad1/Actividad1.java
+++ lib/src/main/java/actividad1/Actividad1.java
@@ -6,11 +6,16 @@ import java.util.stream.Collectors;
 public class Actividad1 {
 
   public List<String> actividad(List<String> stringList) {
-    return null;
+    List<String> listFilter = filter(stringList);
+    return reverseStringList(listFilter);
   }
 
   private List<String> filter(List<String> stringList) {
     return stringList.stream().filter(string -> string.contains("y")).collect(Collectors.toList());
+  }
+
+  private List<String> reverseStringList(List<String> stringList) {
+    return stringList.stream().map(string -> new StringBuilder(string).reverse().toString()).collect(Collectors.toList());
   }
 
 }
